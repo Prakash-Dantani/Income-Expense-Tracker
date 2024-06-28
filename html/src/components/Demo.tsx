ce8e5a97cf8ca978c6ae223d7e28be1dff8b062d
Prakash Dantani <110656797+Prakash-Dantani@users.noreply.github.com>
2024-06-28 08:47:42 +0530

    Prepare project for deployment

--- html/src/components/Demo.tsx
+++
@@ -1,5 +0,0 @@
-const Demo = () => {
-  return <div>Home Page Demo</div>;
-};
-
-export default Demo;
--- html/src/components/utills/CheckFutureDate.tsx
+++
@@ -1,7 +0,0 @@
-const CheckFutureDate = (value) => {
-  const enteredDate = new Date(value);
-  const currentDate = new Date();
-  return enteredDate > currentDate ? "Future dates are invalid" : true;
-};
-
-export default CheckFutureDate;
