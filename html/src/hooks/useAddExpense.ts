75937f93df4eec4cd15490b5467d565c2151f875
Prakash Dantani <110656797+Prakash-Dantani@users.noreply.github.com>
2024-07-07 10:28:41 +0530

    Exclude Html

--- html/src/hooks/useAddExpense.ts
+++
@@ -1,7 +0,0 @@
-import { Expenses } from "../entities/Expenses";
-import APIClient from "../services/APIClient";
-
-const apiClient = new APIClient<Expenses>("/expense/store");
-const useAddExpenses = async (values) => await apiClient.post(values);
-
-export default useAddExpenses;
--- html/src/pages/Untitled-1.js
+++
@@ -1,3 +0,0 @@
-"https://media.licdn.com/dms/image/D4D03AQFbWNb7WurpnA/profile-displayphoto-shrink_100_100/0/1704605586826?e=1714608000&v=beta&t=Sa4PPCpmgj7u8ty4tkzXYRqFXgvBdL7FGwWj1jjkaEQ"
-
-const Links = ["Dashboard", "Projects", "Team", "Contact Us", "About Us"];
